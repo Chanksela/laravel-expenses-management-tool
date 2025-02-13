b4d25ac302709a84c005f1e5f0a0cb23db4a99a1
chanksela <kakhachankseliani@gmail.com>
2025-02-13 18:02:32 +0400

    chore: * added 4 main route * added blank pages for routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+// welcome page
 Route::get('/', function () {
     return view('welcome');
+});
+
+// login page
+Route::get("/login", function(){
+    return view('login');
+});
+
+// register page
+Route::get("/register", function(){
+    return view('register');
+});
+
+// dashboard
+Route::get("/dashboard", function(){
+    return view('dashboard');
 });
