d1c235938344d2053a4990a6ee9720582f09110b
chanksela <kakhachankseliani@gmail.com>
2025-03-06 20:12:59 +0400

    route grouping. custom middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias(
+            ['redirectIfAuthenticated' => (\App\Http\Middleware\RedirectAuthenticatedUsers::class)]
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -6,24 +6,21 @@ use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 // welcome page
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+    Route::get('/', function () {
+        return view('welcome');
+    })->middleware('redirectIfAuthenticated')->name('home');
 
-// login page
-Route::get("/login", [LoginController::class, 'index'])->name('login');
-Route::post("/login", [LoginController::class, 'login'])->name('login.authenticate');
+// auth
+Route::middleware('guest')->group(function(){
+
+
+    Route::get("/login", [LoginController::class, 'index'])->name('login');
+    Route::post("/login", [LoginController::class, 'login'])->name('login.authenticate');
+
+    Route::get("/register", [RegisterController::class, 'index'])->name('register.index');
+    Route::post("/register", [RegisterController::class, 'store'])->name('register.store');
+});
 Route::post("/logout", [LoginController::class, 'logout'])->name('login.logout');
 
-// register page
-Route::get("/register", [RegisterController::class, 'index'])->name('register.index');
-Route::post("/register", [RegisterController::class, 'store'])->name('register.store');
-
-// dashboard
-Route::get("/transactions", [TransactionController::class, 'index'])->middleware('auth')->name('transactions.index');
-Route::get("/transactions/create", [TransactionController::class, 'create'])->middleware('auth')->name('transactions.create');
-Route::post("/transactions/store", [TransactionController::class, 'store'])->middleware('auth')->name('transactions.store');
-Route::get('/transactions/{transaction}/edit', [TransactionController::class, 'edit'])->middleware('auth')->name('transactions.edit');
-Route::put('/transactions/{transaction}', [TransactionController::class, 'update'])->middleware('auth')->name('transactions.update');
-Route::delete("/transactions/{transaction}", [TransactionController::class, 'destroy'])->middleware('auth')->name('transactions.destroy');
-
+// transactions
+Route::resource('transactions', TransactionController::class)->middleware('auth');
